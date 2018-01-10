69acee5e51397cee22718a46a01cc75045a78780
MB <bardwell@ualberta.ca>
2018-01-10 02:32:07 -0700

    Upgraded CSV to mat to allow for missed file numbers (useful when we collect data like test_1f, test_2f, test_4f, ...etc)

--- CSVstomat.m
+++ CSVstomat.m
@@ -3,9 +3,18 @@ NoSets = 25;
 
 for i = 1:NoSets
     x = num2str(i);
-    filename = strcat('Jan05_test1_', x, 'f.csv');
-    load(filename); % Read in data
+    if i < 10
+        x = strcat('0',x);
+    end
+    filename = strcat('Jan05_test4_', x, 'f.csv');
+    
+    if exist(filename, 'file') == 0
+      % File does not exist
+      % Skip to bottom of loop and continue with the loop
+      continue;
+    end
+    load(filename); % Read in datas
 end
 
 clear NoSets; clear x; clear filename; clear i;
-save('OutFilenametobeChanged.mat'); %Change output filename as necessary
+save('OutputFilename.mat'); %Change output filename as necessary
