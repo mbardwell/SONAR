71a1af22001ee8fdc1089d6334ac95bf2820ef91
MB <bardwell@ualberta.ca>
2018-05-09 17:12:57 -0600

    Keil files - working timer/LED with interrupt

---
+++ uvisonadctest/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adcplswork' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+
+#endif /* RTE_COMPONENTS_H */
