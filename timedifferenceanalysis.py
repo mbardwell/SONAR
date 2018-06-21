c5686c871d6feea469836973236b438094f33df3
MB <bardwell@ualberta.ca>
2018-06-21 16:47:14 -0600

    Cleaned it up. Can choose between visually finding time diff or using correlation (seems to be off by a factor of 10 rn)

--- timedifferenceanalysis.py
+++ timedifferenceanalysis.py
@@ -2,9 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-#filename = r"C:\Users\Michael\Documents\SONAR\Data\forphaseshiftanalysis_sampletime2us.csv"
-filename = r"C:\Users\Michael\Downloads\June21_phasetest_0.csv"
-
+filename = r"C:\Users\Michael\Downloads\June21_phasetest_45.csv"
 
 hydrophonea = []; hydrophoneb = []
 with open(filename, 'r') as file:
@@ -22,22 +20,46 @@ Fs = 500000; T = 1/Fs; L = int(hydrophonea.size)
 ##Time signal synthesis
 t = np.arange(0,L)*T
 
-##Range selection
-start = 0.005; end = start + 0.00005
-xcoords = [start, end]
-signalrange = np.arange(int(start*Fs),int(end*Fs))
-
-##Cut signals
-tcut = t[signalrange]
-acut = hydrophonea[signalrange]
-bcut = hydrophoneb[signalrange]
-
+##Plot full signal
 plt.plot(t,hydrophonea,t,hydrophoneb)
-for xc in xcoords:
-    plt.axvline(x=xc)
-plt.show()
 
-plt.plot(tcut,acut,tcut,bcut)
-plt.axvline(0.005024)
-plt.axvline(0.005026)
-plt.show()
+def correlate():
+    ##Range selection
+    start = 0.004; end = start + 0.003
+    xcoords = [start, end]
+    signalrange = np.arange(int(start*Fs),int(end*Fs)) 
+    cut(signalrange)
+    for xc in xcoords:
+        plt.axvline(x=xc)
+    plt.show()
+    plt.plot(tcut,acut,tcut,bcut)
+    plt.show()
+    print(min(np.abs(np.correlate(acut,bcut,"full"))))
+    
+def visual():
+    ##Range selection
+    start = 0.004; end = start + 0.00005
+    xcoords = [start, end]
+    signalrange = np.arange(int(start*Fs),int(end*Fs)) 
+    cut(signalrange)
+    for xc in xcoords:
+        plt.axvline(x=xc)
+    plt.show()
+    plt.plot(tcut,acut,tcut,bcut)
+    plt.axvline(0.004024)
+    plt.axvline(0.00403)
+    plt.show()
+    
+def cut(signalrange):
+    global tcut, acut, bcut
+    tcut = t[signalrange]
+    acut = hydrophonea[signalrange]
+    bcut = hydrophoneb[signalrange]
+
+def main():
+    correlate()
+#    visual()
+    return 0;
+
+if __name__ == "__main__":
+    main()
