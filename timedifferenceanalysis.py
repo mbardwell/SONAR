4a2782265c04953d3ead9130fa5dc1569df842d6
MB <bardwell@ualberta.ca>
2018-06-22 11:52:06 -0600

    Worked out phase, time difference, path length difference calculations

--- timedifferenceanalysis.py
+++ timedifferenceanalysis.py
@@ -15,17 +15,15 @@ with open(filename, 'r') as file:
 file.close()
 
 ##FFT constants
-Fs = 500000; T = 1/Fs; L = int(hydrophonea.size)
+Fs = 500000; T = 1/Fs; L = int(hydrophonea.size); fc = 27000
 
 ##Time signal synthesis
 t = np.arange(0,L)*T
 
-##Plot full signal
-plt.plot(t,hydrophonea,t,hydrophoneb)
-
 def correlate():
+    plt.plot(t,hydrophonea,t,hydrophoneb)
     ##Range selection
-    start = 0.004; end = start + 0.003
+    start = 0.003; end = start + 0.004
     xcoords = [start, end]
     signalrange = np.arange(int(start*Fs),int(end*Fs)) 
     cut(signalrange)
@@ -34,9 +32,11 @@ def correlate():
     plt.show()
     plt.plot(tcut,acut,tcut,bcut)
     plt.show()
-    print(min(np.abs(np.correlate(acut,bcut,"full"))))
+    print(min(np.abs(np.correlate(acut/max(acut),bcut/max(acut),"full"))))
+    print(np.correlate(acut/max(acut),bcut/max(acut),"full"))
     
 def visual():
+    plt.plot(t,hydrophonea,t,hydrophoneb)
     ##Range selection
     start = 0.004; end = start + 0.00005
     xcoords = [start, end]
@@ -47,18 +47,44 @@ def visual():
     plt.show()
     plt.plot(tcut,acut,tcut,bcut)
     plt.axvline(0.004024)
-    plt.axvline(0.00403)
+    timedifference = 0.0000068
+    plt.axvline(0.004024 + timedifference)
     plt.show()
+    phaseshift = 360*timedifference*fc
+    print('Time difference: ', timedifference, 's')
+    print('Phase shift: ', phaseshift, 'deg')
+    print('Path length difference: ', pathlengthdiff(timedifference), 'mm')
+    
+def example():
+    # Demonstrates relationship between phase shift and time delay
+    Fs = 500000; T = 1/Fs; fc = 27000; phaseshift = 75
+    t = np.arange(0,0.0001,T)
+    wavea = 0.5*np.sin(2*np.pi*fc*t + phaseshift*np.pi/180)
+    waveb = 0.5*np.sin(2*np.pi*fc*t)
+    print(np.correlate(wavea, waveb))
+    plt.plot(t,wavea,t,waveb)
+    plt.axvline(0.000026)
+    timedifference = phaseshift/(360*fc)
+    plt.axvline(0.000026 + timedifference) # td = ps/2pif
+    plt.show()
+    print('Time difference: ', timedifference, 's')
+    print('Phase shift: ', phaseshift, 'deg')
+    print('Path length difference: ', pathlengthdiff(timedifference), 'mm')
     
 def cut(signalrange):
     global tcut, acut, bcut
     tcut = t[signalrange]
     acut = hydrophonea[signalrange]
     bcut = hydrophoneb[signalrange]
+    
+def pathlengthdiff(timediff):
+    vsoundwater = 1484 # m/s
+    return vsoundwater*timediff
 
 def main():
-    correlate()
-#    visual()
+#    correlate()
+    visual()
+#    example()
     return 0;
 
 if __name__ == "__main__":
