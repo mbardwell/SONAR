4d91dae9772a0d5ad2152b1ca450b2b672bbd220
MB <bardwell@ualberta.ca>
2018-07-17 16:17:01 -0600

    Put code into classes. Got example started. Some renaming

---
+++ findheading.py
@@ -0,0 +1,89 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from signalanalysis import PhaseShiftAnalysis, TimeDifferenceAnalysis, SignalConditioning
+
+def main():
+    filename = r"C:\Users\Michael\Downloads\July4_phasetest_45.csv"
+    signalA, signalB, signalC = extractfile(filename)
+    
+    tda = TimeDifferenceAnalysis()
+    psa = PhaseShiftAnalysis()
+    sig = SignalConditioning(500000, signalA, signalB, signalC)
+    sig.removebias()
+    sig.cutoutping()
+    sig.normalise()
+#    sig.hamming()
+#    sig.butter_bandpass_filter(order=2)
+#    sig.interpolate(5)
+    psa.fftanalysis(sig.signalA)
+    plt.plot(sig.signalA)
+
+## Use mock data ##
+def sinewave(t, phase):
+    f_centre = 27000
+    wave = t
+    wave = 0.01*np.sin(2*np.pi*t[0:int(len(t)*0.6)]*10)
+    wave = np.append(wave,np.sin(2*np.pi*t[0:int(len(t)*0.4)]*f_centre + phase))
+    wave = np.append(wave,0.01*np.sin(2*np.pi*t[0:int(len(t)*0.6)]*10))
+    return wave
+
+## Use real data ##
+def extractfile(filename):
+    hydrophonech1 = []; hydrophonech2 = []; hydrophonech3 = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['CH1'] == 'Volt':
+                continue
+            hydrophonech1 = np.append(hydrophonech1, float(row['CH3']))
+            hydrophonech2 = np.append(hydrophonech2, float(row['CH1']))
+            hydrophonech3 = np.append(hydrophonech3, float(row['CH2']))
+#        tp = np.linspace(0,1000/500000,num=1000,endpoint=True)
+#        hydrophonech1 = sinewave(tp, 0)
+#        hydrophonech2 = sinewave(tp, 30)
+#        hydrophonech3 = sinewave(tp, 45)
+    file.close()
+    return hydrophonech1, hydrophonech2, hydrophonech3
+    
+if __name__ == "__main__":
+    main()
+    
+#### Plotting functions ####
+#    def plotinputs(cutrange):
+#        plt.plot(t,hydrophonech1,t,hydrophonech2,t,hydrophonech3)
+#        plt.axvline(x=cutrange[0]*T)
+#        plt.axvline(x=cutrange[-1:]*T)
+#        plt.show()
+    
+#    def corrplotting(corrL, corrR):
+#        maxcorrL = np.argmax(np.abs(corrL))
+#        maxcorrR = np.argmax(np.abs(corrR))
+#        trangeL = np.arange(int(0.8*maxcorrL),int(1.2*maxcorrL))
+#        trangeR = np.arange(int(0.8*maxcorrR),int(1.2*maxcorrR))
+#        plt.plot(trangeL, corrL[trangeL], trangeR, corrR[trangeR])
+#        plt.axvline(x=maxcorrL)
+#        plt.axvline(x=maxcorrR)
+#        plt.show()
+#        return maxcorrL, maxcorrR
+    
+#    def blowup(t,signalh1,signalh2,signalh3,minrange=0.4,maxrange=0.5,xline1=None,xline2=None):
+#        tnew = np.arange(int(len(tping)*minrange), int(len(tping)*maxrange))
+#        plt.plot(t[tnew], signalh1[tnew], t[tnew], signalh2[tnew], t[tnew], signalh3[tnew])
+#        if xline1 or xline2 != None:
+#            centre = t[tnew[int(len(tnew)/2)]]
+#            plt.axvline(x=centre)
+#            plt.axvline(x=centre+xline1*T)
+#            plt.axvline(x=centre+xline2*T)
+#        plt.show()
+
+
+#TO DO
+#1. Import data
+#2. Remove DC bias
+#3. Cutout ping
+#4. Interpolate
+#5. Bandpass filter
+#6. Multiply by Hamming factor (?)
+#
+#Apply perfect sine waves and check FFT amplitude/phase information	
---
+++ signalanalysis.py
@@ -0,0 +1,211 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy.signal import butter, lfilter
+
+
+#### Signals have to be conditioned together for findping() reasons ####
+class SignalConditioning(object):
+    def __init__(self, Fs, signalA, signalB, signalC):
+        self.t = np.linspace(0,signalA.size/Fs,signalA.size)
+        self.signalA = signalA
+        self.signalB = signalB
+        self.signalC = signalC
+        self.Fs = Fs
+        self.sizecheck()
+        
+    def sizecheck(self):
+        if self.signalA.size != self.signalB.size:
+            print("signal A not the same size as signal B")
+            exit(1)
+        if self.signalB.size != self.signalC.size:
+            print("signal B not the same size as signal C")
+            exit(1)
+        
+    def removebias(self):        
+        if 1.1 > np.mean(self.signalA) > 0.9:
+            self.signalA = self.signalA - np.mean(self.signalA)
+        if 1.1 > np.mean(self.signalB) > 0.9:
+            self.signalB = self.signalB - np.mean(self.signalB)
+        if 1.1 > np.mean(self.signalA) > 0.9:
+            self.signalC = self.signalC - np.mean(self.signalC)
+            
+    def findping(self):
+        mean = np.mean(np.abs(self.signalA))
+        pingdex = [x for x in self.signalA if x > 2*mean]
+        pingmin = np.min(np.where(self.signalA == pingdex[0]))
+        pingmax = np.max(np.where(self.signalA == pingdex[-1:]))
+        if (pingmax - pingmin)/self.Fs > 0.004:
+            pingmax = int(pingmin + 0.004*self.Fs)
+        return np.arange(pingmin, pingmax)
+    
+    def cutoutping(self):
+        signalrange = self.findping()
+        self.t = self.t[signalrange]
+        self.signalA = self.signalA[signalrange]
+        self.signalB = self.signalB[signalrange]
+        self.signalC = self.signalC[signalrange]
+    
+    def normalise(self):
+        self.signalA = self.signalA/np.max(self.signalA)
+        self.signalB = self.signalB/np.max(self.signalB)
+        self.signalC = self.signalC/np.max(self.signalC)
+        
+    def hamming(self):
+        hamm = np.hamming(self.t.size)
+        self.signalA = self.signalA*hamm
+        self.signalB = self.signalB*hamm
+        self.signalC = self.signalC*hamm
+    
+    def butter_bandpass(self, lowcut, highcut, order=5):
+        nyq = 0.5 * self.Fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band')
+        return b, a
+
+    def butter_bandpass_filter(self, lowcut=20000, highcut=50000, order=5):
+        b, a = self.butter_bandpass(lowcut, highcut, order=order)
+        self.signalA = lfilter(b, a, self.signalA)
+        self.signalB = lfilter(b, a, self.signalB)
+        self.signalC = lfilter(b, a, self.signalC)
+    
+    def interpolate(self, mult_factor):
+        fa = interp1d(self.t, self.signalA, kind='cubic')
+        fb = interp1d(self.t, self.signalB, kind='cubic')
+        fc = interp1d(self.t, self.signalC, kind='cubic')
+        self.t = np.linspace(self.t[0],self.t[-1:],num=mult_factor*len(self.t),endpoint=True)
+        self.signalA = fa(self.t)
+        self.signalB = fb(self.t)
+        self.signalC = fc(self.t)
+
+class PhaseShiftAnalysis(object):
+    def __init__(self, Fc=27000, Fs=500000):
+        self.Fc = Fc
+        self.Fs = Fs
+        
+    def returnangles(self, angles, freq):
+        index = self.binselection(27000, freq)
+        print(angles[index[0]])
+        #self.pilist()
+        
+    def pilist(self):
+        print('pi/2: ', np.pi/2, "\n", 'pi/3: ', np.pi/3, '\n', 'pi/4: ', np.pi/4)
+    
+    def binselection(self, fc, freq):
+        return np.where(freq == min(freq, key=lambda x:abs(x-fc)))
+        
+    def fftanalysis(self, hydrophone):
+        self.T = 1/self.Fs
+        self.L = hydrophone.size
+        ##FFT analysis
+        sp = np.fft.rfft(hydrophone) # compute the fast fourier transform of the signal
+        P2 = np.abs(sp/self.L) # compute two sided spectrum
+        P1 = P2[1:int((self.L/2)+1)] # select the single sided spectrum ignoring DC
+        P1[2:-1] = 2*P1[2:-1]; # I don't understand this step
+        freq = np.fft.rfftfreq(self.L, d=self.T)
+        ang = np.angle(sp)
+        plt.figure(1)
+        plt.plot(freq[0:int(self.L/2)], P1)
+        plt.xlim([20000, 50000])
+        plt.plot()
+        plt.figure(2)
+        plt.plot(freq[0:int(self.L/2)], ang[0:int(self.L/2)])
+        plt.xlim([20000, 50000])
+        plt.plot()
+        self.returnangles(ang, freq)
+        return ang[P1.argmax()]
+        
+    def example(self):
+        ##FFT constants
+        Fs = 500000; T = 1/Fs; L = 1000 # L same as in oscdata
+        
+        ##Signal synthesis
+        t = np.arange(0,L)*T
+        a = 0.5*np.cos(2*np.pi*27000*t + np.pi/2) # used in examples
+        plt.plot(t[0:int(L/5)],a[0:int(L/5)]); plt.show()
+        
+        ##FFT analysis
+        sp = np.fft.rfft(a) # compute the fast fourier transform of the signal
+        P2 = np.abs(sp/L) # compute two sided spectrum
+        P1 = P2[1:int((L/2)+1)] # select the single sided spectrum ignoring DC
+        P1[2:-1] = 2*P1[2:-1]; # I don't understand this step [2:-1 = 2:end]
+        freq = np.fft.rfftfreq(L, d=T)
+        ang = np.angle(sp, deg=1)
+        plt.plot(freq[0:int(L/2)], P1); plt.show()
+        plt.plot(freq, ang, 'o'); plt.show()
+        self.returnangles(ang, freq)    
+        return 0
+#    
+#    def oscdata():
+#        hydrophonea = []; hydrophoneb = []
+#        with open(filename, 'r') as file:
+#            reader = csv.DictReader(file)
+#            for row in reader:
+#                if row['CH1'] == 'Volt':
+#                    continue
+#                hydrophonea = np.append(hydrophonea, float(row['CH1']))
+#                hydrophoneb = np.append(hydrophoneb, float(row['CH2']))
+#        file.close()
+#        
+#        ##FFT constants
+#        Fs = 500000; T = 1/Fs; L = int(hydrophonea.size)
+#        
+#        ##Time signal synthesis
+#        t = np.arange(0,L)*T; print(t[1])
+#        plt.plot(t,hydrophonea,t,hydrophoneb)
+#        
+#        ##Range selection
+#        start = 0.004; end = start + 0.001
+#        xcoords = [start, end]
+#        signalrange = np.arange(int(start*Fs),int(end*Fs))
+#        cut(signalrange, t, hydrophonea, hydrophoneb)
+#        for xc in xcoords:
+#            plt.axvline(x=xc)
+#        plt.show()
+#        plt.plot(tcut,acut,tcut,bcut)
+#        plt.show()
+#        anglea = fftanalysis(acut, Fs)
+#        angleb = fftanalysis(bcut, Fs)
+#        print("Phase shift: ", (anglea - angleb)*180/np.pi)
+#        plt.show()
+#        return 0
+#    
+
+    
+
+    
+class TimeDifferenceAnalysis(object):
+    def __init__(self, Fc=27000, Fs=500000):
+        self.Fc = Fc
+        self.Fs = Fs
+    
+    def correlate(self, signalA, signalB):        
+        corrarray = np.correlate(signalA,signalB,"full")
+        correlation = np.argmax(np.abs(corrarray)) - corrarray.size()
+        return correlation
+        
+    def timedifference(self, correlation):
+        timediff = correlation/self.Fs #/interp_factor
+        return timediff
+        
+    def pathlengthdiff(self, timediff):
+        vsoundwater = 1484 # m/s
+        return vsoundwater*timediff
+    
+#    def phaseshift(self, timediff): ## Functionality overlap with PSA
+#        shift = 360*timediff*self.Fc
+#        return shift
+    
+    
+    
+### For testing ###
+            
+#def main():
+#    psa = PhaseShiftAnalysis()
+##    oscdata()
+#    psa.example()
+#    return 0;
+#
+#if __name__ == "__main__":
+#    main()
